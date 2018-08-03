3a8df0fbe2f492e10ba9a6c4fa0aa0dcdd5ab3fa
SURYAKANT <suryakant@MacBook-Air.local>
2018-08-03 10:53:36 +0530

    removing the white spaces

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,12 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
-    
 /**
 *@param someone the name of the perosn
 *@return gretting string
 */
- 
   public final String greet( final String someone) {
     return String.format("Hello, %s!", someone);
   }
